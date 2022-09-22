64814a5c96662b8d9c97fbb7ae88fa7e1eb04388
olliedirker <olliedirker@gmail.com>
2022-09-21 20:31:24 -0500

    finished manager

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const emailValidator = require("email-validator");
 const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/intern")
+const Intern = require("./lib/Intern")
+const Manager = require("./lib/Manager")
 const fs = require("fs");
