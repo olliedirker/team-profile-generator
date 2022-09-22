bb58475d1665df07411cd50dad5d336216db4c74
olliedirker <olliedirker@gmail.com>
2022-09-21 20:18:12 -0500

    added feature intern and added it to the index

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require("inquirer");
 const emailValidator = require("email-validator");
 const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/intern")
 const fs = require("fs");
